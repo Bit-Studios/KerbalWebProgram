271d41be20f000c4cdf2a40dbd1b2bc0c6c17b90
Nathan Peake <37817019+NathanPeake@users.noreply.github.com>
2023-02-27 03:25:40 +0000

    Api endpoint system added. trying to fix some errors will push fix soon

---
+++ KerbalWebProgram/ApiEndpointsBuiltIn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KerbalWebProgram;
+using KSP;
+using KSP.Game;
+using KSP.Sim.Definitions;
+
+namespace KerbalWebProgram.KerbalWebProgram
+{
+    public static class ApiEndpointsBuiltIn
+    {
+        public static void Init()
+        {
+            KerbalWebProgramMod.webAPI.Add("getCelestialBodyData", new getCelestialBodyData());
+        }
+    }
+    internal class getCelestialBodyData : KWPapi
+    {
+        public override ApiResponseData Run(ApiRequestData apiRequestData)
+        {
+            ApiResponseData apiResponseData = new ApiResponseData();
+            apiResponseData.ID = apiRequestData.ID;
+            apiResponseData.Type = "response";
+
+            CelestialBodyCore celestialBodyCore = GameManager.Instance.Game.CelestialBodies.Get(apiRequestData.paramters["name"].ToString());
+            
+            apiResponseData.Data.Add("bodyName", celestialBodyCore.data.bodyName);
+            apiResponseData.Data.Add("assetKeyScaled", celestialBodyCore.data.assetKeyScaled);
+            apiResponseData.Data.Add("assetKeySimulation", celestialBodyCore.data.assetKeySimulation);
+            apiResponseData.Data.Add("bodyDisplayName", celestialBodyCore.data.bodyDisplayName);
+            apiResponseData.Data.Add("bodyDescription", celestialBodyCore.data.bodyDescription);
+            apiResponseData.Data.Add("isStar", celestialBodyCore.data.isStar);
+            apiResponseData.Data.Add("isHomeWorld", celestialBodyCore.data.isHomeWorld);
+            apiResponseData.Data.Add("navballSwitchAltitudeHigh", celestialBodyCore.data.navballSwitchAltitudeHigh);
+            apiResponseData.Data.Add("navballSwitchAltitudeLow", celestialBodyCore.data.navballSwitchAltitudeLow);
+            apiResponseData.Data.Add("hasSolidSurface", celestialBodyCore.data.hasSolidSurface);
+            apiResponseData.Data.Add("hasOcean", celestialBodyCore.data.hasOcean);
+            apiResponseData.Data.Add("HasLocalSpace", celestialBodyCore.data.HasLocalSpace);
+            apiResponseData.Data.Add("radius", celestialBodyCore.data.radius);
+            apiResponseData.Data.Add("gravityASL", celestialBodyCore.data.gravityASL);
+            apiResponseData.Data.Add("oceanAltitude", celestialBodyCore.data.oceanAltitude);
+            apiResponseData.Data.Add("oceanDensity", celestialBodyCore.data.oceanDensity);
+            apiResponseData.Data.Add("MinTerrainHeight", celestialBodyCore.data.MinTerrainHeight);
+            apiResponseData.Data.Add("MaxTerrainHeight", celestialBodyCore.data.MaxTerrainHeight);
+            apiResponseData.Data.Add("TerrainHeightScale", celestialBodyCore.data.TerrainHeightScale);
+            apiResponseData.Data.Add("TimeWarpAltitudeOffset", celestialBodyCore.data.TimeWarpAltitudeOffset);
+            apiResponseData.Data.Add("SphereOfInfluenceCalculationType", celestialBodyCore.data.SphereOfInfluenceCalculationType);
+            apiResponseData.Data.Add("hasSolarRotationPeriod", celestialBodyCore.data.hasSolarRotationPeriod);
+
+            return new ApiResponseData();
+        }
+    }
+}
--- KerbalWebProgram/KerbalWebProgramMod.cs
+++ KerbalWebProgram/KerbalWebProgramMod.cs
@@ -1,28 +1,39 @@
 using UnityEngine;
-using KSP.Game;
-using System.Collections.Generic;
-using System.Linq;
 using SpaceWarp.API.Mods;
-using System;
-using System.IO;
 using System.Net;
-using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Text.RegularExpressions;
-
+using KerbalWebProgram;
+using KerbalWebProgram.KerbalWebProgram;
+using Newtonsoft.Json;
 
 namespace KerbalWebProgram
 {
+    public class ApiRequestData
+    {
+        public string ID { get; set; }
+        public string Action { get; set; }
+        public Dictionary<string, object> paramters  { get; set; }
+    }
+    public class ApiResponseData
+    {
+        public string Type { get; set; }
+        public string ID { get; set; }
+        public Dictionary<string, object> Data { get; set; }
+    }
+    public abstract class KWPapi
+    {
+        public abstract ApiResponseData Run(ApiRequestData apiRequestData);
+    }
     [MainMod]
     public class KerbalWebProgramMod : Mod
     {
         private bool IsWebLoaded = false;
 
-        public Dictionary<string,string> webAPI = new Dictionary<string,string>();
+        public static Dictionary<string, KWPapi> webAPI = new Dictionary<string, KWPapi>();
+
         public override void OnInitialized()
         {
-            
+            ApiEndpointsBuiltIn.Init();
             Logger.Info("Mod is initialized");
         }
         void Awake()
@@ -38,8 +49,6 @@ namespace KerbalWebProgram
                     IsWebLoaded = true;
                     WebServer webServer = new WebServer();
                     webServer.Start();
-                    WebSocketServer webSocketServer = new WebSocketServer();
-                    webSocketServer.start();
                 }
             }
         }
@@ -47,32 +56,10 @@ namespace KerbalWebProgram
         {
 
         }
-        internal class WebSocketServer
-        {
-            List<TcpClient> clients = new List<TcpClient>();
-            public WebSocketServer() { }
-            internal async void start()
-            {
-
-                TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
-                server.Start();
-                Debug.Log("Listening");
-
-                for (; ; )
-                {
-                    TcpClient client = await server.AcceptTcpClientAsync();
-                    clients.Add(client);
-                    new Thread(new WebSocketWorker(client).ProcessRequest).Start();
-                }
-            }
-        }
+        
         internal class WebServer
         {
-
-            public WebServer()
-            {
-
-            }
+            public WebServer(){}
 
             internal async void Start()
             {
@@ -88,68 +75,20 @@ namespace KerbalWebProgram
                 }
             }
         }
-        internal class WebSocketWorker
+        
+        
+        static ApiResponseData ApiHandler(ApiRequestData apiRequestData)
         {
-            TcpClient client { get; set; }
-            
-
-            public WebSocketWorker(TcpClient client)
+            try
             {
-                this.client = client;
+                ApiResponseData apiResponseData = webAPI[apiRequestData.Action].Run(apiRequestData);
+                return apiResponseData;
             }
-
-            internal void ProcessRequest()
+            catch (Exception e)
             {
-                NetworkStream stream = client.GetStream();
-                while (true)
-                {
-                    
-                    while (!stream.DataAvailable) ;
-                    while (client.Available < 3) ;
-
-
-                    byte[] bytes = new byte[client.Available];
-                    stream.Read(bytes, 0, client.Available);
-                    string clientData = Encoding.UTF8.GetString(bytes);
-
-
-                    if (Regex.IsMatch(clientData, "^GET", RegexOptions.IgnoreCase))
-                    {
-                        string swk = Regex.Match(clientData, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
-                        string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-                        byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
-                        string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
-                        byte[] response = Encoding.UTF8.GetBytes(
-                       "HTTP/1.1 101 Switching Protocols\r\n" +
-                       "Connection: Upgrade\r\n" +
-                       "Upgrade: websocket\r\n" +
-                       "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
-
-                        stream.Write(response, 0, response.Length);
-                    }
-                    else
-                    {
-                        bool fin = (bytes[0] & 0b10000000) != 0,
-                        mask = (bytes[1] & 0b10000000) != 0;
-
-                        int opcode = bytes[0] & 0b00001111,
-                        offset = 2;
-
-                        ulong msglen = (ulong)(bytes[1] & 0b01111111);
-
-                        byte[] decoded = new byte[msglen];
-                        byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
-                        offset += 4;
-
-                        for (ulong i = 0; i < msglen; ++i)
-                            decoded[i] = (byte)(bytes[offset + (int)i] ^ masks[i % 4]);
-
-                        string ClientOutputData = Encoding.UTF8.GetString(decoded);
-                        ApiHandler(client, ClientOutputData, stream);
-                    }
-                }
-
+                Debug.Log(e);
             }
+            return null;
         }
         internal class WebWorker
         {
@@ -162,12 +101,41 @@ namespace KerbalWebProgram
 
             internal void ProcessRequest()
             {
+                Stream body = ctx.Request.InputStream;
+                Encoding encoding = ctx.Request.ContentEncoding;
+                StreamReader reader = new StreamReader(body, encoding);
+                string requestBody = reader.ReadToEnd();
                 ctx.Response.SendChunked = false;
-                ctx.Response.ContentType = System.Net.Mime.MediaTypeNames.Text.Html;
+
+                
 
                 using (var stream = ctx.Response.OutputStream)
                 {
-                    string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                    ApiRequestData data = new ApiRequestData();
+                    string responseString = string.Empty;
+                    if (ctx.Request.Url.AbsolutePath.StartsWith("/api") && ctx.Request.ContentType == "application/json")
+                    {
+
+                        Debug.Log("=================="); 
+                        Debug.Log(requestBody);
+                        ctx.Response.ContentType = "application/json";
+                        data = JsonConvert.DeserializeObject<ApiRequestData>(requestBody);
+                        Debug.Log("==================");
+                        Debug.Log(data.ID);
+                        Debug.Log(data.Action);
+                        Debug.Log(data.paramters);
+                        ApiResponseData responseData = ApiHandler(data);
+                        Debug.Log("==================");
+                        Debug.Log(responseData.ID);
+                        Debug.Log(responseData.Data);
+                        Debug.Log(responseData.Type);
+                        responseString = JsonConvert.SerializeObject(responseData);
+                    }
+                    else {
+                        ctx.Response.ContentType = System.Net.Mime.MediaTypeNames.Text.Html;
+                        responseString = $"<HTML><BODY>{ctx.Request.Url.AbsolutePath}</BODY></HTML>";
+                    }
+                    
                     byte[] buffer = Encoding.UTF8.GetBytes(responseString);
                     stream.Write(buffer, 0, buffer.Length);
                     stream.Close();
@@ -177,34 +145,8 @@ namespace KerbalWebProgram
                 ctx.Response.StatusDescription = "OK";
 
             }
-        }
-        public class ApiRequestData
-        {
-            public string ID { get; set; }
-            public string Action { get; set; }
-            public Dictionary<string, dynamic> paramters { get; set; }
-        }
-        public class ApiResponseData
-        {
-            public string Type { get; set; }
-            public string ID { get; set; }
-            public Dictionary<string, dynamic> Data { get; set; }
-        }
-        static byte[] GenerateBytes(string data)
-        {
-            byte[] response = Encoding.UTF8.GetBytes(data);
-            return response;
-        }
-        static void ApiHandler(TcpClient client, string ClientOutputData, NetworkStream stream)
-        {
-            Debug.Log("got to api");
-            stream = client.GetStream();
-            
-            byte[] response = GenerateBytes("aaaaaaaaaaaaa");
-            Debug.Log("sending");
-            stream.Write(response,0, response.Length);
-        }
 
+        }
     }
 
 }
