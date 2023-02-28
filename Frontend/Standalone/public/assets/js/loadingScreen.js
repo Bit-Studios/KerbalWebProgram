b51f6e57ffe3eaded6ca887ced0cf053d36a10d5
SeanErn <seanernstes@gmail.com>
2023-02-27 19:10:16 -0500

    WIP: ajax api calls

--- Frontend/Standalone/public/assets/js/loadingScreen.js
+++ Frontend/Standalone/public/assets/js/loadingScreen.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   function retryConnect() {
     $('.error').replaceWith('<div class="spinner">');
-
+    var numRetries = 5;
     //test WS connection
     var socket = new WebSocket('ws://localhost:8080/');
 
@@ -14,22 +14,31 @@ $(document).ready(function() {
     socket.addEventListener('error', (event) => {
     console.log('WebSocket error: ', event);
 
-      //Fallback to REST API (ceselistialbodydata as example)
+      //Fallback to REST API (gets kerbin as example)
+      
       $.ajax({
-        url: 'http://localhost:8080/api/ceselistialbodydata',
+        location: 'localhost:8080/api',
         type: 'GET',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          "ID": "0001",
+          "Action" : "getCelestialBodyData",
+          "parameters": {
+            "name": "Kerbin"
+          } 
+        }),
         success: function(response) {
-          console.log(response);
+          console.log(`Data about kerbin (CON. TEST): ${response}`);
           $('#loading-screen').hide(); //Hide the loading screen if the server responds with a 200 OK status code
         },
         error: function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR, textStatus, errorThrown);
           // Display an error message if the server responds with a 500 error status code
           $('#status').html('<span>Error: 503 Backend Unavailable<br> Is Kerbal Web Program installed correctly? </span>');
           $('.spinner').replaceWith('<img src="assets/img/x-24.svg" class="error">');
           $('#status').show(); // Show the error message again if there was an error
 
           var countdown = 30;
-          var numRetries = 5;
           var retryInterval = setInterval(function() {
             if (countdown >= 0) {
               $('#countdown').html('Retrying in ' + countdown + ' seconds...');
